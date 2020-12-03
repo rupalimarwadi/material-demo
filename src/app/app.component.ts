9e73d839654e52016fc2cc92a6f3c99be6bdc9c3
rupalimarwadi <rupali0103@gmail.com>
2020-12-03 22:16:55 +0530

    Input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,13 @@ export class AppComponent {
   title = 'material-demo';
   value = 2;
   isChecked = true;
-
+  foods = [
+    { value: 1, viewValue: 'Apple' },
+    { value: 1, viewValue: 'Orange' },
+    { value: 1, viewValue: 'Grapes' },
+    { value: 1, viewValue: 'Litchy' },
+    { value: 1, viewValue: 'Cherry' }
+  ]
   onChange($event) {
     console.log($event);
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 // import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatRadioModule } from '@angular/material/radio';
-
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,10 @@ import { MatRadioModule } from '@angular/material/radio';
     NoopAnimationsModule,
     MatCheckboxModule,
     MatRadioModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
